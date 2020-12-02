b2c60ac3a31826d0c87d3a1f5ff6a0456a851222
johnsmithm <moshnoi2000@gmail.com>
2020-12-02 14:06:15 +0200

    to english

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
 	$('form').each(function(event){
 		//var _this = this;
 		$(this).find('button').click(function(){
-			if(!$(this).parent()[0].checkValidity())return true;
+			// if(!$(this).parent()[0].checkValidity())return true;
 			console.log(1)
-			$(this).parent().html('<h2>Sunteti in lista noastra!</h2>')
+			$(this).parent().html('<h2>You are in our waiting list!</h2>')
 			
 			$('.typeform-share')[0].click();
 			
